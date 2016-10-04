5f42738056fbaaaf35d7ad6f4d8ec8277027241c
Dmitriy Borovikov <dmitry@borovikov.name>
2016-10-04 13:51:13 +0300

    Initial Commit

---
+++ TroodApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TroodApp
+//
+//  Created by Dmitriy Borovikov on 04.10.16.
+//  Copyright © 2016 Dmitriy Borovikov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
