2eb968bb7f9f2c46875046af71314cd164076071
Dmitriy Borovikov <dmitry@borovikov.name>
2016-10-19 23:57:17 +0300

    Filter buttons functionality imitation.

--- TroodApp/FilterViewController.swift
+++ TroodApp/FilterViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 
 class FilterViewController: UIViewController {
 
+    let normalButtonColor = UIColor(red: 32/255, green: 32/255, blue: 32/255, alpha: 1)
+    let selectedButtonColor = UIColor(red: 241/255, green: 148/255, blue: 28/255, alpha: 1)
     
     @IBOutlet var rangeSelectionButtons: [UIButton]!
-    
     @IBOutlet var tag1SelectionButtons: [UIButton]!
+    @IBOutlet var tag2SelectionButtons: [UIButton]!
+    @IBOutlet var tag3SelectionButtons: [UIButton]!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tapRangeSelection(rangeSelectionButtons[0])
+        tapTag1Selection(tag1SelectionButtons[1])
+        tapTag2Selection(tag2SelectionButtons[0])
+        tapTag3Selection(tag3SelectionButtons[2])
 
     }
 
@@ -41,7 +48,40 @@ class FilterViewController: UIViewController {
     }
     */
     
-    
-    
+    func setSelectedButton(selectedButton: UIButton, selection: [UIButton]) {
 
+        for button in selection {
+            button.isSelected = false
+            button.backgroundColor = normalButtonColor
+        }
+        
+        selectedButton.isSelected = true
+        selectedButton.backgroundColor = selectedButtonColor
+
+    }
+
+    
+    
+    @IBAction func tapRangeSelection(_ sender: UIButton) {
+        
+        setSelectedButton(selectedButton: sender, selection: rangeSelectionButtons)
+    }
+    
+    @IBAction func tapTag1Selection(_ sender: UIButton) {
+
+        setSelectedButton(selectedButton: sender, selection: tag1SelectionButtons)
+    }
+    
+    
+    @IBAction func tapTag2Selection(_ sender: UIButton) {
+
+        setSelectedButton(selectedButton: sender, selection: tag2SelectionButtons)
+}
+    
+    
+    @IBAction func tapTag3Selection(_ sender: UIButton) {
+
+        setSelectedButton(selectedButton: sender, selection: tag3SelectionButtons)
+
+    }
 }
